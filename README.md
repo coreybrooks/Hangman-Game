7ea27298574c13d7fab9258925fa5789f782bd03
Corey <coreybrooks2@gmail.com>
2017-06-21 12:23:04 -0500

    just README and minor indentation fixes

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
 # Hangman-Game
 
 ### Overview
-This application was created with Vanilla JS.  The user is prompted to press any key to start the game.  The application chooses a random movie from an array of movie choices.   The user is then shown blank spaces equal in length to the chosen movie title.  The application validates which key is pressed, and only reacts to a key if it is a valid letter.  The chosen letter is only allowed to be pressed once.  If the letter is in the title, the letter is displayed in the corresponding position and a "success" audio tone is emitted.
+Hangman is a rendition of the classic game hangman with a movie title theme.  This application was created with Vanilla JS and is not mobile responsive.  This game must be played on a system that includes a keyboard.
+
+### Functionality
+The user is prompted to press any key to start the game.  The application chooses a random movie from an array of movie choices.   The user is then shown blank spaces equal in length to the chosen movie title.  The application validates which key is pressed, and only reacts to a key if it is a valid letter.  The chosen letter is only allowed to be pressed once. If the letter is in the title, the letter is displayed in the corresponding position and a "success" audio tone is emitted.
 
 If the letter is not in the title, the letter is displayed in the dead letter section and a "fail" audio tone is emitted.  The user can make 10 wrong guesses.  The number of wrong guesses remaining is displayed in the results div.  If the user guesses all letters correctly, a "win" audio tone is emitted, and the win counter increases by one.  If the user guesses 10 wrong letters, a "lose" audio tone is emitted, and the lose counter increases by one.
 
 Regardless of whether the user wins or loses, the movie title and poster are displayed in the results div after the round is completed.
+
+This code was uploaded to Heroku and can be viewed here: [corey-hangman-game](https://corey-hangman-game.herokuapp.com/)
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,3 @@
-//Everything below will be inside a function that runs until the game is reset after a win or loss
-
-// Global variables
-
-//function to play a sound
-
-
-
-
 // array of movie titles
   var movieTitles = ['gladiator', 'twilight', 'cinderella', 'uninhabited', 'inception', 'frozen', 'hancock', 'dracula',
    'titanic', 'aladdin', 'armageddon', 'enchanted', 'taken', 'twins', 'avatar', 'transformers', 'frankenstein'];
@@ -19,7 +10,8 @@
 // movie title chosen by game for the player to guess
 var chosenMovie = movieTitles[Math.floor(Math.random()*movieTitles.length)];
 numberOfRemainingGuesses = 10;
-       // empty array which to push the user choices, in order to keep track of letters already chosen         
+
+// empty array which to push the user choices, in order to keep track of letters already chosen         
 var isPushed = []; 
 var guesses = '';
 var wins = 0, losses = 0, newPrompt = 'GUESS THE LETTERS <br> IN THE MOVIE TITLE';
@@ -29,43 +21,37 @@ var soundClicks = new Audio('assets/images/click.mp3');
 var fail = new Audio('assets/images/fail.wav');
 var badLetter = new Audio('assets/images/badLetter.mp3');
 
-
-
 //Sets the inital blank title so the player can see how many characters are in the title
 title = '';
-for (i=0; i<chosenMovie.length; i++) {title += '_';};
+for (i=0; i<chosenMovie.length; i++) {
+    title += '_';
+};
 document.querySelector('#blanks').innerHTML=title;
-
 
 // When the user presses a key, it will run the following function...
 document.onkeyup = function(event) {
 
-
-// Determine which key was pressed and converts all to lowercase
+  // Determine which key was pressed and converts all to lowercase
   var userGuess = event.key.toLowerCase();
 
-//begins counting the total number of times a key is clicked, to help clear the original prompt
-totalClickCount++
+  //begins counting the total number of times a key is clicked, to help clear the original prompt
+  totalClickCount++
 
-// The following block of statements will execute if the letter has not been chosen yet
-// .. also executes the first time any key is pressed
-
+  // The following block of statements will execute if the letter has not been chosen yet
+  // .. also executes the first time any key is pressed
    if (isPushed.indexOf(userGuess) === -1) {
-
 
         //The press any key prompt will change the first time a player presses any key
         document.getElementById('topLeft').innerHTML = newPrompt;
         document.getElementById('topLeft').style.color = 'green';
 
-        //The following block of statements will execute if more than one button has been pressed, AND the letter is in the letter array
+        //The following block of statements will execute if more than one button has been pressed, 
+        //AND the letter is in the letter array
+        if ((totalClickCount > 1) && (letters.indexOf(userGuess.toUpperCase()) >= 0)) {
 
-      if ((totalClickCount > 1) && (letters.indexOf(userGuess.toUpperCase()) >= 0)) {
-
-
-   	    // push the user choices to the empty array
-        isPushed.push(userGuess);
-        title = '';
-             
+   	      // push the user choices to the empty array
+          isPushed.push(userGuess);
+          title = '';             
 
                 //if the chosen letter is not in the movie title, then decrease the number of guesses remaining
                 //and display the letter in the already guessed section
@@ -77,36 +63,34 @@ totalClickCount++
                 else {
                 soundClicks.play();
                 }
-
                 
-            for (i=0; i<chosenMovie.length; i++) {
-            	//if the chosen letter is in the isPushed array, then concatentate the letter to title, converts all to uppercase
-	           if (isPushed.indexOf(chosenMovie.charAt(i)) !== -1) {
-			        title += chosenMovie.charAt(i).toUpperCase();
-			    }
-			    //otherwise add "-" to the character space in title
-	           else {
-		            title += "_"
-	            }
-            }
+                for (i=0; i<chosenMovie.length; i++) {
+            	   //if the chosen letter is in the isPushed array, then concatentate the letter to 
+                   //title, converts all to uppercase
+	               if (isPushed.indexOf(chosenMovie.charAt(i)) !== -1) {
+			         title += chosenMovie.charAt(i).toUpperCase();
+			       }
+			       //otherwise add "-" to the character space in title
+	               else {
+		             title += "_"
+	               }
+                }
 
-        //displays the progress in the blanks div and alreadyGuessed div
-        document.querySelector('#blanks').innerHTML=title;
-    }
-
+                //displays the progress in the blanks div and alreadyGuessed div
+                document.querySelector('#blanks').innerHTML=title;
+        }
 
         var html = "<p>Remaining Guesses: " + numberOfRemainingGuesses + "</p>";
     	  document.getElementById('remaining-guesses').innerHTML=html;
           document.getElementById('alreadyGuessed').innerHTML=guesses.toUpperCase();
-console.log(chosenMovie);
+          console.log(chosenMovie);
 
-
-// The section below resets the play area and adds a win to the win counter if the player gets all the letters
-
+        // The section below resets the play area and adds a win to the win counter if the 
+        //player gets all the letters
         if (title.indexOf('_') === -1) {    //if no more blanks are in the title
-         // PlaySound
+          // PlaySound
           sound.play();
-        	wins++;
+          wins++;
           var source = 'assets/images/' + chosenMovie + '.jpg'; //uses the current chosenMovie to set a variable
                                                                 //to change the movie image and title in side panel
           document.getElementById('movieImage').src = source;
@@ -123,18 +107,18 @@ console.log(chosenMovie);
 	           for (i=0; i<chosenMovie.length; i++) {
 	           	title += "_";
 	           }
+
 	        document.getElementById('blanks').innerHTML=title;  
 	        document.getElementById('alreadyGuessed').innerHTML=guesses.toUpperCase();
-	            	  var html = "<p>Remaining Guesses: " + numberOfRemainingGuesses + "</p>";
+
+	        var html = "<p>Remaining Guesses: " + numberOfRemainingGuesses + "</p>";
 	        document.getElementById('remaining-guesses').innerHTML=html;
   	    }
 
-// The section below resets the play area and in the case that the player uses all of their guesses
-
+        // The section below resets the play area and in the case that the player uses all of their guesses
         if (numberOfRemainingGuesses === 0) {
-
-            fail.play();
-            losses++;
+          fail.play();
+          losses++;
           var source = 'assets/images/' + chosenMovie + '.jpg'; //uses the current chosenMovie to set a variable
           console.log(source);                                  //to change the movie image and title in side panel
           document.getElementById('movieImage').src = source;
@@ -146,19 +130,20 @@ console.log(chosenMovie);
 	        numberOfRemainingGuesses=10;
 	        title = ''; 
 
-          var winsDisplay = "<p>WINS: " + wins + "</p>" + "<p>LOSSES: " + losses + "</p>"; 
+            var winsDisplay = "<p>WINS: " + wins + "</p>" + "<p>LOSSES: " + losses + "</p>"; 
 	        document.getElementById('winsDiv').innerHTML=winsDisplay;  //reset the display areas
 
 
 	           for (i=0; i<chosenMovie.length; i++) {
 	           	title += "_";
 	           }
+
 	        document.getElementById('blanks').innerHTML=title;  
 	        document.getElementById('alreadyGuessed').innerHTML=guesses.toUpperCase();
-                  var html = "<p>Remaining Guesses: " + numberOfRemainingGuesses + "</p>";
+            var html = "<p>Remaining Guesses: " + numberOfRemainingGuesses + "</p>";
 	        document.getElementById('remaining-guesses').innerHTML=html;
   	    }
-   }
+    }
 }
 
 
